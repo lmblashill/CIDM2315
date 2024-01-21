cdda96e47b563d8ac9121d3ebb87340567dd8755
lmblashill <156394141+lmblashill@users.noreply.github.com>
2024-01-21 10:50:37 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     double x = 2.5;
+     double y = 3.3;
+     double z = (4*x*x) + (3*y);
+     Console.WriteLine(z);
+     
+
+    }
+}
