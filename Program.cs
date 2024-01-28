a7051105d2d6056cb1a5d8c0ba2603824ff95886
lmblashill <156394141+lmblashill@users.noreply.github.com>
2024-01-28 17:23:59 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
-﻿using System.IO.Compression;
-using System.Security.Cryptography.X509Certificates;
-
-namespace Homework1;
+﻿namespace Homework2;
 
 class Program
 {
     static void Main(string[] args)
     {
-     double x = 2.5;
-     double y = 3.3;
-     double z = (4*x*x) + (3*y);
-     Console.WriteLine(z);
-     
+        Console.WriteLine("Please input your grade letter");
+        string gpa = Console.ReadLine();
+        
+        
+        if(gpa == "A")
+        {
+            Console.WriteLine("GPA is 4");
+        }
+        else if (gpa == "B")
+        {
+            Console.WriteLine("GPA is 3");
+        }
+        else if (gpa == "C")
+        {
+            Console.WriteLine("GPA is 2");
+        }    
+        else if (gpa == "D")
+        {
+            Console.WriteLine("GPA is 1");
+        }
+        else if (gpa == "F") 
+        {
+            Console.WriteLine("GPA is 0");
+        }
+        else
+        { 
+            Console.WriteLine("GPA is invalid:");
+        }
 
     }
-}
+
+
+
+        }
+    
+
