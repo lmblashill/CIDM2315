9077bb613214d29f161887c5b5baffc258821146
lmblashill <156394141+lmblashill@users.noreply.github.com>
2024-02-04 19:44:05 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,21 @@
-﻿namespace Homework2;
+﻿using System.Globalization;
+
+namespace Lecture3;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Please input your grade letter");
-        string gpa = Console.ReadLine();
-        
-        
-        if(gpa == "A")
-        {
-            Console.WriteLine("GPA is 4");
-        }
-        else if (gpa == "B")
-        {
-            Console.WriteLine("GPA is 3");
-        }
-        else if (gpa == "C")
-        {
-            Console.WriteLine("GPA is 2");
-        }    
-        else if (gpa == "D")
-        {
-            Console.WriteLine("GPA is 1");
-        }
-        else if (gpa == "F") 
-        {
-            Console.WriteLine("GPA is 0");
-        }
-        else
-        { 
-            Console.WriteLine("GPA is invalid:");
-        }
-
-    }
-
-
-
-        }
     
-
+    static void Main()
+    {
+  Console.WriteLine("please input an integer");
+  int N = Convert.ToInt16(Console.ReadLine());
+  for(int i = 0; i < N; i++)
+  {
+    for (int j =0; j < N; j++)
+    {
+        Console.Write("#");
+    }
+  Console.WriteLine();
+  }
+}
+}
